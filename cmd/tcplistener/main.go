518bd610e6f83a2102d65c7b0628b0659617a7bb
BaDelhommer <badelhommer@gmail.com>
2025-03-19 21:52:29 -0500

    compeleted connect the parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,46 +1,44 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
+	"httpfromtcp/internal/request"
 	"log"
 	"net"
-	"strings"
 )
 
 const port = ":42069"
 
-func getLinesChannel(conn io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer close(lines)
-		defer conn.Close()
-		currentLineContents := ""
-		for {
-			b := make([]byte, 8)
-			n, err := conn.Read(b)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			str := string(b[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
-}
+// func getLinesChannel(conn io.ReadCloser) <-chan string {
+// 	lines := make(chan string)
+// 	go func() {
+// 		defer close(lines)
+// 		defer conn.Close()
+// 		currentLineContents := ""
+// 		for {
+// 			b := make([]byte, 8)
+// 			n, err := conn.Read(b)
+// 			if err != nil {
+// 				if currentLineContents != "" {
+// 					lines <- currentLineContents
+// 				}
+// 				if errors.Is(err, io.EOF) {
+// 					break
+// 				}
+// 				fmt.Printf("error: %s\n", err.Error())
+// 				return
+// 			}
+// 			str := string(b[:n])
+// 			parts := strings.Split(str, "\n")
+// 			for i := 0; i < len(parts)-1; i++ {
+// 				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
+// 				currentLineContents = ""
+// 			}
+// 			currentLineContents += parts[len(parts)-1]
+// 		}
+// 	}()
+// 	return lines
+// }
 
 func main() {
 	l, err := net.Listen("tcp", port)
@@ -57,11 +55,14 @@ func main() {
 		fmt.Printf("listen assep: %v\n", conn.LocalAddr())
 		fmt.Println("=====================================")
 
-		outChan := getLinesChannel(conn)
-
-		for line := range outChan {
-			fmt.Printf("%s\n", line)
+		request, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("Request bad: %s", err)
 		}
+
+		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
+			request.RequestLine.Method, request.RequestLine.RequestTarget, request.RequestLine.HttpVersion)
+
 		fmt.Println("connection to ", conn.RemoteAddr(), "yeeted")
 	}
 
