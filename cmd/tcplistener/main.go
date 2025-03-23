3655301e2e63cda0eaa845ba8da19d347ecbeeae
BaDelhommer <badelhommer@gmail.com>
2025-03-23 12:10:46 -0500

    completed live headers

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -66,5 +66,10 @@ func main() {
 		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n",
 			request.RequestLine.Method, request.RequestLine.RequestTarget, request.RequestLine.HttpVersion)
 
+		fmt.Println("Headers:")
+		for key, value := range request.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
+
 	}
 }
